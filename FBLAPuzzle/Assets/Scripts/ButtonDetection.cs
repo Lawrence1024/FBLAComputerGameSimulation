ada12923000f4ccb54203bde6aceadcc942401ca
Karen Shieh <karen93shieh@gmail.com>
2021-02-10 23:50:17 -0800

    main page

---
+++ FBLAPuzzle/Assets/Scripts/ButtonDetection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class ButtonDetection : MonoBehaviour
+{
+    public GameObject PlayButton;
+    public GameObject InstructionButton;
+    public GameObject LeaderBoardButton;
+    //public GameObject PlayButton;
+    //public GameObject PlayButton;
+    //public GameObject PlayButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayButton.GetComponent<Button>().onClick.AddListener(executePlayButton);
+        InstructionButton.GetComponent<Button>().onClick.AddListener(executeInstructionButton);
+        LeaderBoardButton.GetComponent<Button>().onClick.AddListener(executeLeaderBoardButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void executePlayButton() {
+        Debug.Log("Play");
+    }
+    void executeInstructionButton() {
+        Debug.Log("Instruction");
+    }
+    void executeLeaderBoardButton() {
+        Debug.Log("Leader Board");
+
+    }
+}
---
+++ FBLAPuzzle/Assets/Scripts/sceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneManager : MonoBehaviour
+{
+    public GameObject LoadingCanvas;
+    public GameObject PauseMenuCanvas;
+    public GameObject MainPageCanvas;
+    public GameObject InstructionCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadingCanvas.SetActive(false);
+        PauseMenuCanvas.SetActive(false);
+        InstructionCanvas.SetActive(false);
+        MainPageCanvas.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void activatePauseMenu() { 
+        
+    }
+
+}
