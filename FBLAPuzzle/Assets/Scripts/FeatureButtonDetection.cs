63fd17d0cd942dcfa13ee5ba6ea1f69f2a8cf197
Karen Shieh <karen93shieh@gmail.com>
2021-02-12 13:34:44 -0800

    level components

---
+++ FBLAPuzzle/Assets/Scripts/FeatureButtonDetection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatureButtonDetection : MonoBehaviour
+{
+    LevelManager levelManager;
+    public GameObject tipButton;
+    public GameObject NextPageButton;
+    public GameObject LastPageButton;
+    int pageCounter = 0;
+    void Start()
+    {
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        tipButton.SetActive(false);
+        if (pageCounter <= 0)
+        {
+            LastPageButton.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void activateTips() {
+        Debug.Log("Tips"); 
+        pageCounter = 0;
+        LastPageButton.SetActive(false);
+        levelManager.changeInstrucitonPage(pageCounter);
+        levelManager.InstructionCanvas.SetActive(true);
+    }
+    public void executeNextPageButton()
+    {
+        pageCounter++;
+        Debug.Log("page Counter" + pageCounter + " instructionpages " + levelManager.TipPages.Length);
+        if (pageCounter > 0)
+        {
+            LastPageButton.SetActive(true);
+        }
+        if (pageCounter == (levelManager.TipPages.Length - 1))
+        {
+            NextPageButton.SetActive(false);
+        }
+        levelManager.changeInstrucitonPage(pageCounter);
+        Debug.Log("Next");
+
+    }
+    public void executeLastPageButton()
+    {
+        pageCounter--;
+        if (pageCounter <= 0)
+        {
+            LastPageButton.SetActive(false);
+        }
+        if (pageCounter < levelManager.TipPages.Length - 1)
+        {
+            NextPageButton.SetActive(true);
+        }
+        levelManager.changeInstrucitonPage(pageCounter);
+
+        Debug.Log("Last");
+
+    }
+    public void executeExistButton(GameObject existObj)
+    {
+        Debug.Log("Exist");
+        existObj.SetActive(false);
+
+    }
+}
---
+++ FBLAPuzzle/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject LoadingCanvas;
+    public GameObject PauseMenuCanvas;
+    public GameObject LevelCanvas;
+    public GameObject InstructionCanvas;
+    public GameObject ScoreboardCanvas;
+    public GameObject[] TipPages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadingCanvas.SetActive(false);
+       // PauseMenuCanvas.SetActive(false);
+        InstructionCanvas.SetActive(false);
+        ScoreboardCanvas.SetActive(false);
+        LevelCanvas.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void changeInstrucitonPage(int pageNum)
+    {
+        for (int i = 0; i < TipPages.Length; i++)
+        {
+            TipPages[i].SetActive(false);
+        }
+        TipPages[pageNum].SetActive(true);
+    }
+}
