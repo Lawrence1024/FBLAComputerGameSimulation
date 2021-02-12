c06a6d39a705019ee87afb6f0d90cf5a3628e9a9
Lawrence Shieh <lawrenceandaugy@gmail.com>
2021-02-11 17:29:45 -0800

    basic movement

--- FBLAPuzzle/Assets/Scripts/PlayerController.cs
+++ FBLAPuzzle/Assets/Scripts/PlayerController.cs
@@ -11,12 +11,33 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
+        movePoint.position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Input.GetAxisRaw("Horizontal") == 1f)
+            {
+                movePoint.position += new Vector3(0.99f, 0f, 0f);
+            }
+            if (Input.GetAxisRaw("Horizontal") == -1f)
+            {
+                movePoint.position += new Vector3(-0.99f, 0f, 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") == 1f)
+            {
+                movePoint.position += new Vector3(0f, 0.99f, 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") == -1f)
+            {
+                movePoint.position += new Vector3(0f, -0.99f, 0f);
+            }
+        }
     }
 }
