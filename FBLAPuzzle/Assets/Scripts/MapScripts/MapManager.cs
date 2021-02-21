3ee12d2dd99c3136490d927b83e96cf7cc83f9cb
Karen Shieh <karen93shieh@gmail.com>
2021-02-20 16:16:51 -0800

    misc

--- FBLAPuzzle/Assets/Scripts/MapScripts/MapManager.cs
+++ FBLAPuzzle/Assets/Scripts/MapScripts/MapManager.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
     public GameObject LoadingCanvas;
     public GameObject PauseMenuCanvas;
     public GameObject MapCanvas;
+    public GameObject[] allLevelButtons;
+    private Account activeAccount;
     void Start()
     {
         LoadingCanvas.SetActive(false);
+        activeAccount = GameObject.Find("AccountsManager").GetComponent<AccountsManager>().activeAccount;
+        for (int i=0; i<allLevelButtons.Length; i++) {
+            allLevelButtons[i].GetComponent<Button>().interactable = false;
+        }
+        allLevelButtons[0].GetComponent<Button>().interactable = true;
+        for (int i = 1; i < activeAccount.starsList.Count; i++)
+        {
+            //Debug.Log(i+" "+activeAccount.starsList[i]);
+            Debug.Log(i+" "+activeAccount.starsList[i-1]);
+            if (activeAccount.starsList[i-1]>=0) {
+                allLevelButtons[i].GetComponent<Button>().interactable = true;
+            }
+        }
     }
 
     // Update is called once per frame
