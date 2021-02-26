fe29aff3514e37806bdcf3a98c65f8d4554ff7df
Karen Shieh <karen93shieh@gmail.com>
2021-02-26 03:15:36 -0800

    Update DontDestroy.cs

--- FBLAPuzzle/Assets/Scripts/DontDestroy.cs
+++ FBLAPuzzle/Assets/Scripts/DontDestroy.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿//FileName: DontDestroy.cs
+//FileType: C# File
+//Author: Karen Shieh, Lawrence Shieh
+//Date: Feb. 26, 2021
+//Description: DontDestroy preserves the gameobject that the script is attatched to. 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
     public static DontDestroy instance;
-    
+    /* Method Name: Start()
+     * Summary: Check if the object exist or not. If so, destroy the duplicate object.
+     * @param N/A
+     * @return N/A
+     * Special Effects: Destroy the duplicate object if needed.
+     */
     void Start()
     {
         DontDestroyOnLoad(this);
@@ -16,9 +26,7 @@ public class DontDestroy : MonoBehaviour
         {
             DestroyObject(gameObject);
         }
-        
     }
-
     // Update is called once per frame
     void Update()
     {
