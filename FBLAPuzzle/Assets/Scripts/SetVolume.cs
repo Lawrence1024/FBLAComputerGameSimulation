fd28a875e11c1ed28d77bf52cc16b3459767fcdd
Karen Shieh <karen93shieh@gmail.com>
2021-02-26 03:13:19 -0800

    Update SetVolume.cs

--- FBLAPuzzle/Assets/Scripts/SetVolume.cs
+++ FBLAPuzzle/Assets/Scripts/SetVolume.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//FileName: SetVolume.cs
+//FileType: C# File
+//Author: Karen Shieh, Lawrence Shieh
+//Date: Feb. 26, 2021
+//Description: SetVolume changes the volume size. 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -10,13 +15,17 @@ public class SetVolume : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
+    /* Method Name: modifyVolume(float sliderValue)
+     * Summary: Change the audio mixer's value according to the sliderValue.
+     * @param sliderValue: The slider's value
+     * @return N/A
+     * Special Effects: Change the audio mixer's value according to the sliderValue.
+     */
     public void modifyVolume(float sliderValue) {
         GameObject.Find("AudioPlayer").GetComponent<PlayAudio>().setScrollBarVal(sliderValue);
         audioMixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
