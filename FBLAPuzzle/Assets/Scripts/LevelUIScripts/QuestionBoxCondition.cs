4cbd3cc8e2ff43d17daa167207f6b6b4f11aae81
Karen Shieh <karen93shieh@gmail.com>
2021-02-26 02:44:03 -0800

    Update QuestionBoxCondition.cs

--- FBLAPuzzle/Assets/Scripts/LevelUIScripts/QuestionBoxCondition.cs
+++ FBLAPuzzle/Assets/Scripts/LevelUIScripts/QuestionBoxCondition.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//FileName: QuestionBoxCondition.cs
+//FileType: C# File
+//Author: Karen Shieh, Lawrence Shieh
+//Date: Feb. 26, 2021
+//Description: QuestionBoxCondition contains the function to display the questions and check if the correct answer is chosen or not.   
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
@@ -14,7 +19,6 @@ public class QuestionBoxCondition : MonoBehaviour
     FeatureButtonDetection featureButtonDetection;
     public bool activated = false;
     public int QuestionNumber;
-
     string question;
     string answer1;
     string answer2;
@@ -22,7 +26,12 @@ public class QuestionBoxCondition : MonoBehaviour
     string answer4;
     string correctAnswer;
     private GameObject[] buttons;
-
+    /* Method Name: Start()
+     * Summary: Get the game object "LevelManager"'s script "QuestionInteraction" (a script attatched to the LevelManager) and other game elements. 
+     * @param N/A
+     * @return N/A
+     * Special Effects: Assign variables to different game elements.
+     */
     void Start()
     {
         questionInteraction= GameObject.Find("LevelManager").GetComponent<QuestionInteraction>();
@@ -30,17 +39,21 @@ public class QuestionBoxCondition : MonoBehaviour
         featureButtonDetection = GameObject.Find("LevelManager").GetComponent<FeatureButtonDetection>();
         buttons = levelManager.buttons;
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
+    /* Method Name: checkBoxQuestionStatus()
+     * Summary: If the question box wasn't answered yet, it set the question canvas to active. It calls functions in questionInteraction
+     *          scripts to get the string value to display on the buttons and question panel, and check which button is correct or not.
+     * @param N/A
+     * @return N/A
+     * Special Effects: Assign different texts to buttons and question panel.
+     */
     public void checkBoxQuestionStatus()
     {
         if (!activated) {
-
             activated = true;
             levelManager.QuestionCanvas.transform.GetChild(1).gameObject.GetComponent<Image>().color = new Vector4(1, 1, 1, 1);
             levelManager.QuestionCanvas.transform.GetChild(2).gameObject.GetComponent<Image>().color = new Vector4(1, 1, 1, 1);
@@ -57,12 +70,10 @@ public class QuestionBoxCondition : MonoBehaviour
             questionInteraction.AnswerButton2.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = answer2;
             questionInteraction.AnswerButton3.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = answer3;
             questionInteraction.AnswerButton4.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = answer4;
-
             checkCorrectAnswer(questionInteraction.AnswerButton1, 1);
             checkCorrectAnswer(questionInteraction.AnswerButton2, 2);
             checkCorrectAnswer(questionInteraction.AnswerButton3, 3);
             checkCorrectAnswer(questionInteraction.AnswerButton4, 4);
-
             for (int i = 0; i < buttons.Length; i++)
             {
                 buttons[i].GetComponent<Button>().interactable = false;
@@ -70,8 +81,13 @@ public class QuestionBoxCondition : MonoBehaviour
             levelManager.QuestionCanvas.SetActive(true);
         }
     }
-
-
+    /* Method Name: checkCorrectAnswer(GameObject button, int buttonNum)
+     * Summary: Check if the button is the correct answer or not.
+     * @param button: The answer button.
+     * @param buttonNum: The number of the answer button.
+     * @return N/A
+     * Special Effects: Assign right or wrong to the button's RightOrWrong variable.
+     */
     void checkCorrectAnswer(GameObject button, int buttonNum) {
 
         if (buttonNum == int.Parse(correctAnswer)) {
