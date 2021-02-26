cc1e57422edd322eb87d9049d89bd6bed8465d40
Karen Shieh <karen93shieh@gmail.com>
2021-02-26 03:41:37 -0800

    Update T_PointsCalculation.cs

--- FBLAPuzzle/Assets/Scripts/Tutorial/T_PointsCalculation.cs
+++ FBLAPuzzle/Assets/Scripts/Tutorial/T_PointsCalculation.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections;
+﻿//FileName: T_PointsCalculation.cs
+//FileType: C# File
+//Author: Karen Shieh, Lawrence Shieh
+//Date: Feb. 26, 2021
+//Description: T_PointsCalculation contains the function to count down points in a game level (for tutorial).  
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class T_PointsCalculation : MonoBehaviour
 {
-    // Start is called before the first frame update
     T_LevelManager levelManager;
     T_FeatureButtonDetection featureButtonDetection;
     public int points = 1000;
     public bool levelComplete = false;
     public bool stopTime = true;
+    /* Method Name: Start()
+     * Summary: Set the poitns value on the canvas to the input points (1000). Call pointsCountDown() to count down the points.
+     * @param N/A
+     * @return N/A
+     * Special Effects: Initial display of the points. 
+     */
     void Start()
     {
         levelManager = GameObject.Find("LevelManager").GetComponent<T_LevelManager>();
@@ -22,13 +32,18 @@ public class T_PointsCalculation : MonoBehaviour
             stopTime = false;
         }
     }
-
     // Update is called once per frame
     void Update()
     {
 
     }
-
+    /* Method Name: pointsCountDown()
+     * Summary: The point value minus 1 every .12 seconds. If the game is paused or completed or the points value is <=0 the counter
+     *          stops counting down.
+     * @param N/A
+     * @return N/A
+     * Special Effects: Display of the points. 
+     */
     IEnumerator pointsCountDown()
     {
         if (!stopTime)
