fd4aa3cbc482b3c5e7ce75b37fe2a3807cb4029f
Karen Shieh <karen93shieh@gmail.com>
2021-02-26 01:26:37 -0800

    button detection and map manager commented

--- FBLAPuzzle/Assets/Scripts/MapScripts/MapManager.cs
+++ FBLAPuzzle/Assets/Scripts/MapScripts/MapManager.cs
@@ -23,12 +23,13 @@ public class MapManager : MonoBehaviour
     private Account activeAccount;
     /* Method Name: Start()
      * Summary: Set pause menu and loading canvas to deactivate when the map scene starts. Get the activateAccount through the 
-     *          game object "AccountsManager" and see its progress. According to the progress, the starts and buttons are either
-     *          deactivated/uninteractable or activated/interactable. It also sets the text display on the top left corner to the 
-     *          user's name and the number of stars he/she has.
+     *          game object "AccountsManager" and see its progress. According to the progress, the starts and level buttons are 
+     *          either activated/deactivated, interactable/uninteractable. It also sets the text display on the top left corner 
+     *          on the screen to the user's name and the number of stars he/she has.
      * @param N/A
      * @return N/A
-     * Special Effects: Pause menu and loading .
+     * Special Effects: Pause menu and loading canvas deactivated. Star count and level bottons activated/deactivated, 
+     *                  interactable/uninteractable. Display name and star count on the top left corner of the screen.
      */
     void Start()
     {
@@ -38,7 +39,6 @@ public class MapManager : MonoBehaviour
         for (int i=0; i<allLevelButtons.Length; i++) {
             allLevelButtons[i].GetComponent<Button>().interactable = false;
         }
-
         for (int i = 0; i < starPanels.Length; i++) {
             starPanels[i].transform.GetChild(0).gameObject.SetActive(false);
             starPanels[i].transform.GetChild(1).gameObject.SetActive(false);
@@ -60,13 +60,16 @@ public class MapManager : MonoBehaviour
                 starPanels[i].SetActive(true);
             }
         }
-
         currentUserName.GetComponent<TMPro.TextMeshProUGUI>().text=activeAccount.userName;
         currentStarCount.GetComponent<TMPro.TextMeshProUGUI>().text = activeAccount.getTotalStar().ToString();
         userColor.GetComponent<Image>().color = activeAccount.avatarColor;
     }
-
-    // Update is called once per frame
+    /* Method Name: Update()
+     * Summary: Detect if the escape key is pressed. If so, it calls activatePauseMenu().
+     * @param N/A
+     * @return N/A
+     * Special Effects: Call activatePauseMenu().
+     */
     void Update()
     {
         if (Input.GetKeyDown("escape"))
@@ -74,17 +77,20 @@ public class MapManager : MonoBehaviour
             activatePauseMenu();
         }
     }
+    /* Method Name: activatePauseMenu()
+     * Summary: Activate (if the pause menu is not activated) or deactivate (if the pause men is activated) the pause menu. Also 4
+     *          update the volume slider's value to the current volume. If the pasue menu is not activated it calls changeVolume(float val)
+     *          in the game object "AudioPlayer" to set the volume to 1. 
+     * @param N/A
+     * @return N/A
+     * Special Effects: Activate/deactivate the pause menu and change volume of background music.
+     */
     void activatePauseMenu()
     {
-
         PauseMenuCanvas.SetActive(!PauseMenuCanvas.activeSelf);
         PauseMenuCanvas.transform.GetChild(2).gameObject.GetComponent<Slider>().value = GameObject.Find("AudioPlayer").GetComponent<PlayAudio>().sliderValue;
-
-        if (PauseMenuCanvas.activeSelf)
+        if (!PauseMenuCanvas.activeSelf)
         {
-            //GameObject.Find("AudioPlayer").GetComponent<PlayAudio>().changeVolume(0.25f);
-        }
-        else {
             GameObject.Find("AudioPlayer").GetComponent<PlayAudio>().changeVolume(1f);
         }
     }
